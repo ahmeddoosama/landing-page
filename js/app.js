80575179b7d8f316a9f9e2526d815e63700ebdc3
Ahmed Osama <ahmedosamabedawy@gmail.com>
2020-09-04 07:55:07 +0200

    Edit in app.js file

--- js/app.js
+++ js/app.js
@@ -34,13 +34,16 @@ function getActiveElement() {
     minValue = 1000000
     for (section of SECTIONS) {
         let bounding = section.getBoundingClientRect()
-        if (bounding.top > -300 && bounding.top < minValue) {
+        if (bounding.top > -200 && bounding.top < minValue) {
             minValue = bounding.top
             maxValue = section
+            maxValue.classList.add('your-active-class')
         }
     }
+
     return maxValue
 }
+
 
 /**
  * End Helper Functions
@@ -48,47 +51,39 @@ function getActiveElement() {
  * 
  */
 
-// build the nav
-function navbarItems() {
-    for(let section of SECTIONS) {
-        let navbar__item = document.createElement("li")
-        navbar__item.className = "menu__link"
-        navbar__item.dataset.nav = section.id
-        navbar__item.innerText = section.dataset.nav
-        NAVBAR.appendChild(navbar__item)
-    }
-}
+[...SECTIONS].map(section =>
+    NAVBAR.innerHTML += `
+    <li class="menu__link active__link" data-nav="${section.id}" >
+        ${section.getAttribute('data-nav')}
+    </li>`
+)
+
 
 // Add class 'active' to section when near top of viewport
-function setActiveClass() {
-    window.addEventListener('scroll', function(event) {
+window.addEventListener('scroll', () => {
 
-        // Add active class on section
-        let section = getActiveElement();
-        section.classList.add('your-active-class')
+    // Add active class on section
+    let section = getActiveElement();
 
-        // Remove other section active
-        for (let item of SECTIONS) {
-            if (item.id != section.id && item.classList.contains('your-active-class')) {
-                item.classList.remove('your-active-class')
-            }
+    // Remove other section active
+    for (let item of SECTIONS) {
+        if (item.id != section.id && item.classList.contains('your-active-class')) {
+            item.classList.remove('your-active-class')
         }
+    }
 
-        // set 'style' to corresponding header
-        let activeItem = document.querySelector('li[data-nav="' + section.id + '"]')
-        activeItem.classList.add('active__link')
+    // set 'style' to corresponding header
+    let activeItem = document.querySelector('li[data-nav="' + section.id + '"]')
+    activeItem.classList.add('active__link')
 
-        // remove 'style' from other header
-        let items = document.querySelectorAll('.menu__link');
-        for (let item of items) {
-            if (item.dataset.nav != activeItem.dataset.nav & item.classList.contains('active__link')) {
-                item.classList.remove('active__link');
-            }
-        };
-    })
-}
-
-
+    // remove 'style' from other header
+    let items = document.querySelectorAll('.menu__link');
+    for (let item of items) {
+        if (item.dataset.nav != activeItem.dataset.nav & item.classList.contains('active__link')) {
+            item.classList.remove('active__link');
+        }
+    };
+})
 
 // Scroll to anchor ID using scrollTO event
 function scrollToEvent() {
@@ -104,11 +99,5 @@ function scrollToEvent() {
  * 
  */
 
-// Build menu
-navbarItems();
-
 // Scroll to section on link click
 scrollToEvent();
-
-// Set sections as active
-setActiveClass();
